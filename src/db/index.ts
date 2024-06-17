1d9898d005015ce393c702d9e9f0cb06dbe628cb
Nkachukwu Victor <nkachukwuvictor2016@gmail.com>
2024-06-18 00:11:59 +0100

    updated bug

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,7 +4,7 @@ import env from "@/env";
 
 import * as schema from "./schema";
 
-const connection = await mysql.createConnection({
+const connection = mysql.createPool({
   uri: env.DATABASE_URL,
 });
 
